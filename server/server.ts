23febaceb91fede04e3accfdc7da76e7ada064bc
Novia Lim <novia@hellolanding.com>
2022-06-05 04:45:19 -0700

    github pages test

--- server/server.ts
+++ server/server.ts
@@ -24,7 +24,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.all("*", function (req, res, next) {
   var origin = req.get("origin");
@@ -38,18 +38,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.use(order_routes);
+
+app.get("/", function (req, res) {
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
-});
-
-app.use(order_routes);
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
